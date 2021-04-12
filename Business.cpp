65c9c4b1efed41b2fa6cb17a9c58d7b696af8b3e
carlos137387 <81447417+carlos137387@users.noreply.github.com>
2021-04-12 15:16:10 -0500

    Add files via upload

---
+++ Business.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// Carlos Guerrero
+// COSC 
+
+int main() {}
+    std::cout << "Ginz Ramen Shop" << std::endl;
+    std::cout << "5539 W Loop 1604 N, Alamo Heights, TX 78254" << std::endl;
+    std::cout << "210330991" <<std::endl;
+    return 0;
+
+    }   
