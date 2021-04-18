f1d92dc1850cce72779f99521f2f93d591035653
carlos137387 <81447417+carlos137387@users.noreply.github.com>
2021-04-17 21:20:39 -0500

    Add files via upload

---
+++ Number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int firstnumber , secondnumber , sumOfTwoNumbers;
+    cout << "Enter two integers" ";
+    cin >> firstNumber >> secondNumber
+
+    sumOfTwoNumbers = firstnumber + secondNumber;
+    cout << fiurstnumber << " + " << secondNumber << " = " << sumOfTwoNumbers;
+
+    return 0;
+    
+}
